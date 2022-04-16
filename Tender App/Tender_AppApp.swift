82a0ae9b9e5959cf92d582133c82c8f689128ddc
Roman Rakhlin <rrakhlin@gmail.com>
2022-04-16 14:24:26 +0300

    Initial Commit

---
+++ Tender App/Tender_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tender_AppApp.swift
+//  Tender App
+//
+//  Created by Roman Rakhlin on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Tender_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
